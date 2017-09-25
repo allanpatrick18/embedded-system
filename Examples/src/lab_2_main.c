32ca5e183fded65ab4cf6210148a43b90d64e1ac
Guilherme Jacichen (Notebook) <gui_jaci@yahoo.com.br>
2017-09-25 17:05:08 -0300

    Auto calibração

--- Examples/src/lab_2_main.c
+++ Examples/src/lab_2_main.c
@@ -8,12 +8,17 @@
 #include "ctype.h"
 #include "libdemo.h"
 
-FILE *file;
+FILE *file_gantt;
+
+//************************
+//IDs de Timers
+//************************
+osTimerId id_timer_sampling;
 
 //************************
 //IDs de Threads
 //************************
-osThreadId id_thread_sample_ace;
+osThreadId id_thread_samples;
 osThreadId id_thread_write_ram;
 osThreadId id_thread_bar_red_leds;
 osThreadId id_thread_bar_green_leds;
@@ -35,12 +40,14 @@ osThreadId id_thread_export_file;
 int8_t axis_x[64]={0};
 int8_t axis_y[64]={0};
 int8_t axis_z[64]={0};
+
 int index_x =0;
 int index_y =0;
 int index_z =0;
-int8_t filter_x;
-int8_t filter_y;
-int8_t filter_z;
+
+int8_t filtered_x;
+int8_t filtered_y;
+int8_t filtered_z;
 
 
 //************************
@@ -67,6 +74,13 @@ int8_t formula(int8_t eixo[4]){
 }
 
 //************************
+//Callback de Timers
+void timer_sampling_cb(void const *arg) {
+  osSignalSet(id_thread_samples, 0x1);
+}
+osTimerDef(timer_sampling, timer_sampling_cb);
+
+//************************
 //Threads
 
 //Thread para geração de chaves
@@ -75,41 +89,54 @@ void thread_samples(void const *args){
   int8_t queue_x[4]={0};
   int8_t queue_y[4]={0};
   int8_t queue_z[4]={0};
+
+  int8_t x_off = 0;
+  int8_t y_off = 0;
+  int8_t z_off = 0;
+
+  acc_read(&x_off, &y_off, &z_off);
+
+  x_off = -x_off;
+  y_off = -y_off;
+  z_off = -z_off;
   
   while(1){
-    evt = osSignalWait (0x02, 250);
-    if (evt.status == osEventSignal){
-      osSignalWait (0x00,1000);
-    }else if(evt.status == osEventTimeout || evt.status == osOK){
+    evt = osSignalWait (0x01, osWaitForever);     
+    if(evt.status == osEventSignal){
+      if (evt.value.signals & 0x02){
+        osSignalWait (0x00,1000);
+      }
+      else if (evt.value.signals & 0x01){
       int8_t x = 0;
       int8_t y = 0;
       int8_t z = 0;
       
       acc_read(&x, &y, &z);
       
-      x = -x;
-      y = -y;
-      z = 64-z;
+      x = x+x_off;
+      y = y+y_off;
+      z = z+z_off;
       
       queue_x[3] = queue_x[2];
       queue_x[2] = queue_x[1];
       queue_x[1] = queue_x[0];
       queue_x[0] = x;
-      filter_x =formula(queue_x);
+      filtered_x =formula(queue_x);
       
       queue_y[3] = queue_y[2];
       queue_y[2] = queue_y[1];
       queue_y[1] = queue_y[0];
       queue_y[0] = y;
-      filter_y =formula(queue_y);
+      filtered_y =formula(queue_y);
       
       queue_z[3] = queue_z[2];
       queue_z[2] = queue_z[1];
       queue_z[1] = queue_z[0];
       queue_z[0] = z;
-      filter_z =formula(queue_z);
+      filtered_z =formula(queue_z);
       
       osSignalSet(id_thread_write_ram, 0x1);
+      }
     }
   }
 }
@@ -123,29 +150,14 @@ void thread_write_ram(void const *args){
     if(evt.status == osEventSignal){
       if (evt.value.signals & 0x02){
         osSignalWait (0x00,1000);
-      }else if (evt.value.signals & 0x01){             
-        while(evt.value.signals & 0x08){
-          osSignalClear(osThreadGetId(),0x08);
-          osSignalWait (0x04,osWaitForever);
-        }
-        
-        osSignalSet(id_thread_bar_red_leds, 0x08);
-        osSignalSet(id_thread_bar_green_leds, 0x08);
-        osSignalSet(id_thread_display_oled, 0x08);
-        osSignalSet(id_thread_export_file, 0x08);
-        
+      }else if (evt.value.signals & 0x01){
         index_x =(index_x+63)%64;
         index_y =(index_y+63)%64;
         index_z =(index_z+63)%64;
         
-        axis_x[index_x] = filter_x;
-        axis_y[index_y] = filter_y;
-        axis_z[index_z] = filter_z;
-        
-        osSignalSet(id_thread_bar_red_leds, 0x04);
-        osSignalSet(id_thread_bar_green_leds, 0x04);
-        osSignalSet(id_thread_display_oled, 0x04);
-        osSignalSet(id_thread_export_file, 0x04);
+        axis_x[index_x] = filtered_x;
+        axis_y[index_y] = filtered_y;
+        axis_z[index_z] = filtered_z;
         
         osSignalSet(id_thread_bar_red_leds, 0x1);
         osSignalSet(id_thread_bar_green_leds, 0x1);
@@ -179,12 +191,6 @@ void thread_display_oled(void const *args){
         osSignalWait (0x00,1000);
       }else if (evt.value.signals & 0x01){ 
         oled_clearScreen(OLED_COLOR_WHITE); 
-
-        while(evt.value.signals & 0x08){
-          osSignalClear(osThreadGetId(),0x08);
-          osSignalWait (0x04,osWaitForever);
-        }
-        osSignalSet(id_thread_write_ram, 0x08);
         int last_j = ((axis_z[index_z]+128)*64)/256;
         for (int i=0 ; i< 64;i++){
           int j = axis_z[(i+index_z)%64]+128;
@@ -206,7 +212,6 @@ void thread_display_oled(void const *args){
           oled_putPixel(64, i, OLED_COLOR_BLACK);
           last_j = j;
         }
-        osSignalSet(id_thread_write_ram, 0x04);          
       }
     }
   }
@@ -228,20 +233,19 @@ void thread_main(){
   printf("finished");
 }
 
-
 int main(int n_args, int8_t** args){
   //Inicialização de Kernel vai aqui
   osKernelInitialize();  
+
   //************************
   //Init Diagram of Gantt
   //*********************** 
+  file_gantt = fopen("gantt.txt","w");
+  fprintf(file_gantt,"gantt\n");
+  fprintf(file_gantt,"    title A Gantt Diagram\n");
+  fprintf(file_gantt,"    dateFormat x\n");
+//  fclose(file_gantt);  
   
-//  file = fopen("gantt.txt","w");
-//  fprintf(file,"gantt\n");
-//  fprintf(file,"    title A Gantt Diagram\n");
-//  fprintf(file,"    dateFormat x\n");
-//  fclose(file);  
-
   // Setup IRS
   GPIOInit();
   SSPInit();
@@ -253,11 +257,17 @@ int main(int n_args, int8_t** args){
   acc_init();
   joystick_init();
   setup_isr();
-
+  
+  //************************
+  //Inicialização de Timers aqui
+  //************************
+  id_timer_sampling = osTimerCreate(osTimer(timer_sampling), osTimerPeriodic, NULL);
+  osTimerStart(id_timer_sampling, 50);
+  
   //************************
   //Inicialização de Threads aqui
   //************************
-  id_thread_sample_ace =                osThreadCreate(osThread(thread_samples),        NULL);
+  id_thread_samples =                   osThreadCreate(osThread(thread_samples),        NULL);
   id_thread_write_ram =                 osThreadCreate(osThread(thread_write_ram),      NULL);
   id_thread_bar_red_leds =              osThreadCreate(osThread(thread_red_led),        NULL);
   id_thread_bar_green_leds =            osThreadCreate(osThread(thread_green_led),      NULL);
@@ -277,7 +287,7 @@ int main(int n_args, int8_t** args){
   //Finalização de Threads aqui
   //************************
   osThreadTerminate(id_thread_export_file);
-  osThreadTerminate(id_thread_sample_ace);
+  osThreadTerminate(id_thread_samples);
   osThreadTerminate(id_thread_write_ram);
   osThreadTerminate(id_thread_bar_red_leds);
   osThreadTerminate(id_thread_bar_green_leds);  
